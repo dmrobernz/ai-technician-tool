6fa74c1240feab33763d648f754bce3327447999
Dave Robertson <60903592+dmrobernz@users.noreply.github.com>
2025-05-15 09:49:51 +1200

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+
+// firebaseConfig.js
+const firebaseConfig = {
+  apiKey: "YOUR-API-KEY",
+  authDomain: "YOUR-PROJECT.firebaseapp.com",
+  projectId: "YOUR-PROJECT-ID",
+  storageBucket: "YOUR-PROJECT.appspot.com",
+  messagingSenderId: "YOUR-MESSAGE-ID",
+  appId: "YOUR-APP-ID"
+};
+firebase.initializeApp(firebaseConfig);
